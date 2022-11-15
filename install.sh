f2b17a873189370f23b542612fe41d632b14da73
sagebei <bei.zhou@qmul.ac.uk>
2022-11-15 15:16:14 +0000

    update

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@ rm -rf build
 mkdir build
 
 python_dir=$(which python)
-(cd build && cmake -DCMAKE_INSTALL_PREFIX=~/Installations -DPYTHON_EXECUTABLE=$python_dir ../)
+dir=$(python -m site --user-site)
 
+(cd build && cmake -DCMAKE_INSTALL_PREFIX=~/Installations -DPYTHON_EXECUTABLE=$python_dir ../)
 (cd build && cmake ..)
 (cd build && make)
 
-dir=$(python -m site --user-site)
 if [ ! -d $dir ]; then
   mkdir $dir
 fi
