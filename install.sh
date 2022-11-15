842f1e462f938b3ccc07ecad10fab1ffb79e2125
sagebei <bei.zhou@qmul.ac.uk>
2022-11-15 15:45:44 +0000

    update

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ if [ ! -d $dir ]; then
   mkdir $dir
 fi
 
-(cd build && cmake -DCMAKE_INSTALL_PREFIX=~/Installations -DMUQ_USE_PYTHON=ON -DPYTHON_EXECUTABLE=$python_dir ../)
+(cd build && cmake -DCMAKE_INSTALL_PREFIX=~/Installations -DPYTHON_EXECUTABLE=$python_dir ../)
 (cd build && cmake ..)
 (cd build && make)
 (cd build && mv *.so $dir)
