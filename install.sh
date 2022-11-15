9879f4e9a7301a55e4c459b349d8e2fef8101bc9
sagebei <bei.zhou@qmul.ac.uk>
2022-11-15 15:41:14 +0000

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
-rm -rf build
-mkdir build
+if [ -d "./build" ]; then
+  rm -rf "./build"
+  mkdir "./build"
+else
+  mkdir "./build"
+fi
+
+if [ ! -d "./pybind11" ]; then
+  git clone https://github.com/pybind/pybind11.git
+fi
 
 python_dir=$(which python)
 dir=$(python -m site --user-site)
@@ -12,5 +20,7 @@ fi
 (cd build && cmake ..)
 (cd build && make)
 (cd build && mv *.so $dir)
+
+
 
 
