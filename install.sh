85a490c8a4e7652878821b4d6c163fec1f2d312f
sagebei <bei.zhou@qmul.ac.uk>
2022-11-15 15:21:52 +0000

    update

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ mkdir build
 python_dir=$(which python)
 dir=$(python -m site --user-site)
 
-(cd build && cmake -DCMAKE_INSTALL_PREFIX=~/Installations -DPYTHON_EXECUTABLE=$python_dir ../)
+(cd build && cmake -DCMAKE_INSTALL_PREFIX=~/Installations -DMUQ_USE_PYTHON=ON -DPYTHON_EXECUTABLE=$python_dir ../)
 (cd build && cmake ..)
 (cd build && make)
 
