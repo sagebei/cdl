1424f86b853245d8588bda51c5044101e276942b
sagebei <bei.zhou@qmul.ac.uk>
2022-11-18 12:49:30 +0000

    update

--- install.sh
+++ install.sh
@@ -6,21 +6,19 @@ else
 fi
 
 # change this environment variable to your virtual environment directory, like $HOME'/venv'
-export PYTHONUSERBASE='/opt/anaconda3'   # macos
+#export PYTHONUSERBASE='/opt/anaconda3'   # macos
 #export PYTHONUSERBASE=$HOME'/venv'      # hpc
-#export PYTHONUSERBASE=$HOME"/PycharmProjects/venv"  # ubuntu
+export PYTHONUSERBASE=$HOME"/PycharmProjects/venv"  # ubuntu
 
 if [ ! -d "./pybind11" ]; then
   git clone https://github.com/pybind/pybind11.git
 fi
 
 dir=$(python -m site --user-site)
-
+echo $dir
 if [ ! -d $dir ]; then
   mkdir $dir
 fi
-
-echo $dir
 
 (cd build && cmake ..)
 (cd build && make)
