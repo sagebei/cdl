c35535f0647f08e57ef7e77da12ddf5f3d502038
sagebei <bei.zhou@qmul.ac.uk>
2022-11-21 12:38:16 +0000

    update

--- bind/bind.cpp
+++ bind/bind.cpp
@@ -1,5 +1,5 @@
-#include "../headers/condorcet_domain.h"
-#include "../headers/utils.h"
+#include "condorcet_domain.h"
+#include "utils.h"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
