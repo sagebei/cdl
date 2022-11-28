5ccc6e050d1a5d8b0d2edbfcb7c02de37a407a77
sagebei <bei.zhou@qmul.ac.uk>
2022-11-28 15:10:13 +0000

    update

--- bind/get_started.py
+++ bind/get_started.py
@@ -1,25 +1,31 @@
 from cdl import CondorcetDomain, RuleScheme
 
-cd = CondorcetDomain(n=6)
+# create the CondorcetDomain object
+cd = CondorcetDomain(n=5)
+# create a rule scheme
 rs = RuleScheme()
 rs.add([2, 3], "3N1")
 rs.add([4], "1N3")
-
+# initialize a set of triplet-rules based on this rule scheme
 trs = cd.init_by_scheme(rs)
+# calculate the resulting condorcet domain
 domain = cd.condorcet_domain(trs)
+# print the size of the condorcet domain
 print("Domain size: ", len(domain))
 cd.print_cd(domain)
 
+# calculate its brother domains
 bros = cd.cd_brothers(domain)  # bros include domain itself
 for bro in bros:
     cd.print_cd(bro)
 
-# brother cds have the same hash value
+# hash the brother domains. Brother cds have the same hash value
 for bro in bros:
     cds = cd.cd_brothers(bro)
     print('Bros Hash: ', cd.hash_cd_brothers(cds))
 
-print(cd.subset_cd_sizes(trs, 5))
+# calculate the size of 4 elements subset CDs.
+print(cd.subset_cd_sizes(trs, 4))
 
 
 
