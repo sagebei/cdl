ff66903637d21a3e65caaa27da7f569c0f018def
sagebei <bei.zhou@qmul.ac.uk>
2022-12-01 13:15:58 +0000

    update

--- bind/install.sh
+++ bind/install.sh
@@ -5,6 +5,11 @@ else
   mkdir "./build"
 fi
 
+if [ $(uname) == "Darwin" ]; then
+  export CC=/usr/bin/gcc
+  export CXX=/usr/bin/g++
+fi
+
 if [ ! -d "./pybind11" ]; then
   git clone https://github.com/pybind/pybind11.git
 fi
