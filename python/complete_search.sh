fab6dc66e19e41fcd87b063bcb9f646b2eb97d71
sagebei <bei.zhou@qmul.ac.uk>
2023-04-25 15:21:19 +0800

    update

--- python/complete_search.sh
+++ python/complete_search.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #$ -cwd
-#$ -pe smp 1
-#$ -l h_vmem=1G
 #$ -j y
-#$ -l h_rt=1:0:0
+#$ -pe smp 1            # Request cores (8 per GPU)
+#$ -l h_vmem=2G         # 11G RAM per core
+#$ -l h_rt=1:0:0      # Max 1hr runtime (can request up to 240hr)
 
 module load gcc/12.1.0 cmake
 source ~/venv/bin/activate
