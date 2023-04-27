ce1abd4161923fee75773e88efaf3e1459fabb5b
sagebei <bei.zhou@qmul.ac.uk>
2023-04-27 08:18:34 +0800

    update

--- python/scripts/complete_search.sh
+++ python/scripts/complete_search.sh
@@ -9,4 +9,5 @@
 module load gcc/12.1.0
 source ~/venv/bin/activate
 
-python complete_search.py -n 6 -cutoff 16 -rules "2N3" "2N1" -n_complete 5 -n_cores 8
+python complete_search.py -n 7 -cutoff 16 -rules "2N3" "2N1" "1N3" "3N1" -n_complete 5 -n_cores 16
+
--- python/scripts/parallel_search.sh
+++ python/scripts/parallel_search.sh
@@ -5,9 +5,9 @@
 #$ -l h_vmem=2G
 #$ -j y
 #$ -l h_rt=10:0:0
-#$ -t 1-8
+#$ -t 1-16
 
 module load gcc/12.1.0
 source ~/venv/bin/activate
 
-python parallel_search.py -n 6 -cutoff 16 -rules "2N3" "2N1"  -triplet_id 6 -core_id ${SGE_TASK_ID} &
+python parallel_search.py -n 7 -cutoff 16 -rules "2N3" "2N1" "1N3" "3N1"  -triplet_id 6 -core_id ${SGE_TASK_ID}
