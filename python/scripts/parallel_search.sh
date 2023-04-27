e54119654049a448d66e255bfd0b538c44b485ed
sagebei <bei.zhou@qmul.ac.uk>
2023-04-27 08:21:27 +0800

    update

--- python/scripts/parallel_search.sh
+++ python/scripts/parallel_search.sh
@@ -10,4 +10,4 @@
 module load gcc/12.1.0
 source ~/venv/bin/activate
 
-python parallel_search.py -n 7 -cutoff 16 -rules "2N3" "2N1" "1N3" "3N1"  -triplet_id 6 -core_id ${SGE_TASK_ID}
+python ../parallel_search.py -n 7 -cutoff 16 -rules "2N3" "2N1" "1N3" "3N1"  -triplet_id 6 -core_id ${SGE_TASK_ID}
