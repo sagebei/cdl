755dbeb540e94e756e6a5eff7b052053ecd148e0
sagebei <bei.zhou@qmul.ac.uk>
2023-05-05 12:25:34 +0800

    update

--- hpc/complete_search.sh
+++ hpc/complete_search.sh
@@ -10,7 +10,7 @@
 module load gcc/12.1.0
 source ~/venv/bin/activate
 
-python ../python/complete_search.py -n 8  \
+python ../python/complete_search.py -n 9  \
                                     -cutoff 16  \
                                     -threshold 0  \
                                     -rules "2N3" "2N1" "1N3" "3N1" \
