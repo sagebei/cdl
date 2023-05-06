fe82f061d0c9d28be84615d199a89b1da3d39a84
sagebei <bei.zhou@qmul.ac.uk>
2023-05-06 14:03:34 +0800

    update

--- hpc/get_results.sh
+++ hpc/get_results.sh
@@ -10,12 +10,12 @@
 module load gcc/12.1.0
 source ~/venv/bin/activate
 
-python ../python/get_results.py -n 8  \
+python ../python/get_results.py -n 9  \
                                 -cutoff 16  \
-                                -threshold 100  \
-                                -top_n 10000  \
+                                -threshold 0  \
+                                -top_n 100000  \
                                 -n_complete 7 \
-                                -n_cores 1000  \
+                                -n_cores 2000  \
                                 -rules "2N3" "2N1" "1N3" "3N1" \
                                 -lib_path "/data/home/acw554/cdl" \
                                 -result_path "/data/scratch/acw554/results"
--- hpc/parallel_search.sh
+++ hpc/parallel_search.sh
@@ -5,18 +5,18 @@
 #$ -l h_vmem=8G
 #$ -j y
 #$ -l h_rt=10:0:0
-#$ -t 1-1000
+#$ -t 1-2000
 #$ -N parallel
 
 module load gcc/12.1.0
 source ~/venv/bin/activate
 
-python ../python/parallel_search.py -n 8  \
+python ../python/parallel_search.py -n 9  \
                                     -cutoff 16  \
-                                    -threshold 100 \
-                                    -top_n 10000  \
+                                    -threshold 0  \
+                                    -top_n 100000  \
                                     -n_complete 7 \
-                                    -n_cores 1000 \
+                                    -n_cores 2000  \
                                     -rules "2N3" "2N1" "1N3" "3N1"  \
                                     -lib_path "/data/home/acw554/cdl" \
                                     -result_path "/data/scratch/acw554/results" \
