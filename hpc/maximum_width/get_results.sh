ae9e5d7353f7c52bea63114f042a9f41bea27e49
sagebei <bei.zhou@qmul.ac.uk>
2023-05-06 21:07:35 +0800

    update

--- hpc/maximum_width/get_results.sh
+++ hpc/maximum_width/get_results.sh
@@ -10,13 +10,13 @@
 module load gcc/12.1.0
 source ~/venv/bin/activate
 
-python ~/cdl/get_results.py -n 15  \
-                            -cutoff 16  \
-                            -threshold 0  \
-                            -top_n 1000000000  \
-                            -n_complete 20 \
-                            -n_cores 2000  \
-                            -shuffle 1 \
-                            -rules "2N3" "2N1" \
-                            -lib_path "/data/home/acw554/cdl" \
-                            -result_path "/data/scratch/acw554/results"
+python ~/cdl/python/get_results.py -n 15  \
+                                   -cutoff 16  \
+                                   -threshold 0  \
+                                   -top_n 1000000000  \
+                                   -n_complete 20 \
+                                   -n_cores 2000  \
+                                   -shuffle 1 \
+                                   -rules "2N3" "2N1" \
+                                   -lib_path "/data/home/acw554/cdl" \
+                                   -result_path "/data/scratch/acw554/results"
