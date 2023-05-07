d8eea351b08166242c8b08bfc980df55b9e571bb
sagebei <bei.zhou@qmul.ac.uk>
2023-05-07 08:09:49 +0800

    update

--- hpc/get_results.sh
+++ hpc/get_results.sh
@@ -16,7 +16,7 @@ python ~/cdl/python/get_results.py -n 7  \
                                    -top_n 1000  \
                                    -n_complete 9 \
                                    -n_cores 2000  \
-                                   -n_chunks 2000 \
+                                   -n_chunks 100000 \
                                    -shuffle 0 \
                                    -rules "2N3" "2N1" "1N3" "3N1" \
                                    -lib_path "/data/home/acw554/cdl" \
