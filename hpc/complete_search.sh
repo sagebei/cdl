ae9c36066782d31b6419b2d28c45aa627587eb6a
sagebei <bei.zhou@qmul.ac.uk>
2023-05-07 08:21:11 +0800

    update

--- hpc/complete_search.sh
+++ hpc/complete_search.sh
@@ -15,8 +15,8 @@ python ~/cdl/python/complete_search.py -n 7  \
                                        -threshold 0  \
                                        -top_n 1000  \
                                        -n_complete 9 \
-                                       -n_cores 2000  \
-                                       -n_chunks 100000 \
+                                       -n_cores 1000  \
+                                       -n_chunks 10000 \
                                        -shuffle 0 \
                                        -rules "2N3" "2N1" "1N3" "3N1" \
                                        -lib_path "/data/home/acw554/cdl" \
--- hpc/parallel_search.sh
+++ hpc/parallel_search.sh
@@ -5,7 +5,7 @@
 #$ -l h_vmem=8G
 #$ -j y
 #$ -l h_rt=10:0:0
-#$ -t 1-2000
+#$ -t 1-1000
 #$ -N parallel
 
 module load gcc/12.1.0
@@ -16,8 +16,8 @@ python ~/cdl/python/parallel_search.py -n 7  \
                                        -threshold 0  \
                                        -top_n 1000  \
                                        -n_complete 9 \
-                                       -n_cores 2000  \
-                                       -n_chunks 100000 \
+                                       -n_cores 1000  \
+                                       -n_chunks 10000 \
                                        -shuffle 0 \
                                        -rules "2N3" "2N1" "1N3" "3N1" \
                                        -lib_path "/data/home/acw554/cdl" \
