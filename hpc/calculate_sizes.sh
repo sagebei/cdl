6f64d1681e10b4b3e24a84adbc1752ace1d4f7d5
sagebei <bei.zhou@qmul.ac.uk>
2023-05-09 15:04:50 +0800

    update

--- hpc/calculate_sizes.sh
+++ hpc/calculate_sizes.sh
@@ -11,7 +11,7 @@
 module load gcc/12.1.0
 source ~/venv/bin/activate
 
-python ~/cdl/python/calculate_sizes.py -subfolder_path $1\
+python ~/cdl/python/calculate_sizes.py -folder_path $1\
                                        -n_cores $2 \
                                        -core_id ${SGE_TASK_ID}
 
