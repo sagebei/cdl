65b9940303c0f13dce60dce6102a64ae675325f0
sagebei <bei.zhou@qmul.ac.uk>
2023-05-10 18:49:15 +0800

    update

--- hpc/form_clusters.sh
+++ hpc/form_clusters.sh
@@ -4,8 +4,8 @@
 #$ -j y
 #$ -pe smp 1            # Request cores (8 per GPU)
 #$ -l h_vmem=8G         # 11G RAM per core
-#$ -l h_rt=24:0:0      # Max 1hr runtime (can request up to 240hr)
-#$ -N complete
+#$ -l h_rt=1:0:0      # Max 1hr runtime (can request up to 240hr)
+#$ -N cluster
 
 module load gcc/12.1.0
 source ~/venv/bin/activate
