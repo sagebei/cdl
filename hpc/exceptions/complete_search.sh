6cf8709a79f6336e3e8a22b1634f0d2969587268
sagebei <bei.zhou@qmul.ac.uk>
2023-05-13 07:06:43 +0800

    update

--- hpc/exceptions/complete_search.sh
+++ hpc/exceptions/complete_search.sh
@@ -10,13 +10,13 @@
 module load gcc/12.1.0
 source ~/venv/bin/activate
 
-python ~/cdl/python/complete_search.py -n 9  \
+python ~/cdl/python/complete_search.py -n 12  \
                                        -cutoff 16  \
                                        -threshold 0  \
-                                       -top_n 100000  \
-                                       -n_complete 5 \
+                                       -top_n 1000000  \
+                                       -n_complete 10 \
                                        -n_cores 1000  \
-                                       -n_chunks 1000 \
+                                       -n_chunks 10000 \
                                        -shuffle "." \
                                        -rules "2N3" "2N1" "1N3" "3N1" \
                                        -lib_path "/data/home/acw554/cdl" \
--- hpc/exceptions/parallel_search.sh
+++ hpc/exceptions/parallel_search.sh
@@ -11,13 +11,13 @@
 module load gcc/12.1.0
 source ~/venv/bin/activate
 
-python ~/cdl/python/parallel_search.py -n 9  \
+python ~/cdl/python/parallel_search.py -n 12  \
                                        -cutoff 16  \
                                        -threshold 0  \
-                                       -top_n 100000  \
+                                       -top_n 1000000  \
                                        -n_complete 75 \
                                        -n_cores 1000  \
-                                       -n_chunks 1000 \
+                                       -n_chunks 10000 \
                                        -shuffle "." \
                                        -rules "2N3" "2N1" "1N3" "3N1" \
                                        -lib_path "/data/home/acw554/cdl" \
