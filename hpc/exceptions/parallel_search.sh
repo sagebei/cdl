6094cbe72a62952f072b858b5c7b98f8e81e4348
sagebei <bei.zhou@qmul.ac.uk>
2023-05-12 16:45:57 +0800

    update

--- hpc/exceptions/parallel_search.sh
+++ hpc/exceptions/parallel_search.sh
@@ -14,14 +14,14 @@ source ~/venv/bin/activate
 python ~/cdl/python/parallel_search.py -n 9  \
                                        -cutoff 16  \
                                        -threshold 0  \
-                                       -top_n 10000  \
-                                       -n_complete 13 \
+                                       -top_n 100000  \
+                                       -n_complete 5 \
                                        -n_cores 1000  \
-                                       -n_chunks 100000 \
+                                       -n_chunks 1000 \
                                        -shuffle "." \
                                        -rules "2N3" "2N1" "1N3" "3N1" \
                                        -lib_path "/data/home/acw554/cdl" \
-                                       -result_path "/data/scratch/acw554/results" \
+                                       -result_path "/data/scratch/acw554/exceptions" \
                                        -core_id ${SGE_TASK_ID}
 
 
