8ce060454cd3f6815d4cae47648091e2f4aaaa21
sagebei <bei.zhou@qmul.ac.uk>
2023-05-15 19:59:04 +0800

    update

--- hpc/maximum_width/complete_search.sh
+++ hpc/maximum_width/complete_search.sh
@@ -10,7 +10,7 @@
 module load gcc/12.1.0
 source ~/venv/bin/activate
 
-python ~/cdl/python/complete_search.py -n 14  \
+python ~/cdl/python/complete_search.py -n 15  \
                                        -cutoff 16  \
                                        -threshold 0  \
                                        -top_n 1000000000  \
