987c92c7f62ead59a7d91051ba89f8fd1176f5fe
sagebei <bei.zhou@qmul.ac.uk>
2023-05-15 19:06:00 +0800

    update

--- hpc/maximum_width/calculate_sizes.sh
+++ hpc/maximum_width/calculate_sizes.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-n_cores=$2
-
 #$ -cwd
 #$ -pe smp 1
 #$ -l h_vmem=8G
 #$ -j y
 #$ -l h_rt=24:0:0
-#$ -t 1-n_cores
+#$ -t 1-100
 #$ -N sizes
 
 module load gcc/12.1.0
 source ~/venv/bin/activate
 
 python ~/cdl/python/calculate_sizes.py -folder_path $1 \
-                                       -n_cores n_cores \
+                                       -n_cores 100 \
                                        -core_id ${SGE_TASK_ID}
 
