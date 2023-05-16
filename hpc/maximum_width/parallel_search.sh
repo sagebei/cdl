ebabbfcba11c4c69615b15225339c89770534286
sagebei <bei.zhou@qmul.ac.uk>
2023-05-16 14:35:59 +0800

    update

--- hpc/maximum_width/parallel_search.sh
+++ hpc/maximum_width/parallel_search.sh
@@ -11,11 +11,11 @@
 module load gcc/12.1.0
 source ~/venv/bin/activate
 
-python ~/cdl/python/parallel_search.py -n 18  \
+python ~/cdl/python/parallel_search.py -n 19  \
                                        -cutoff 16  \
                                        -threshold 0  \
                                        -top_n 1000000000  \
-                                       -n_complete 60 \
+                                       -n_complete 70 \
                                        -n_cores 1000  \
                                        -n_chunks 1000000 \
                                        -shuffle 1 \
