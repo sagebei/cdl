98d4a198d26545f80593ab62f38157bade5ae99f
sagebei <bei.zhou@qmul.ac.uk>
2023-05-24 19:32:55 +0800

    update

--- hpc/1N3_3N1/parallel_search.sh
+++ hpc/1N3_3N1/parallel_search.sh
@@ -13,13 +13,13 @@ module load gcc/12.1.0
 
 source ~/venv/bin/activate
 
-python ~/cdl/python/parallel_search.py -n 20  \
+python ~/cdl/python/parallel_search.py -n 12  \
                                        -cutoff 16  \
                                        -threshold 0.9  \
                                        -top_n 1000000000000  \
-                                       -n_complete 90 \
+                                       -n_complete 30 \
                                        -n_cores 1000  \
-                                       -n_chunks 1000000 \
+                                       -n_chunks 100000 \
                                        -shuffle 1 \
                                        -rules "1N3" "3N1" \
                                        -lib_path "/data/home/acw554/cdl" \
