3eac6c70318547da15db5c4e288f4d223dc9ce6d
sagebei <bei.zhou@qmul.ac.uk>
2023-06-05 05:05:53 +0100

    update

--- hpc/exceptions/parallel_search.sh
+++ hpc/exceptions/parallel_search.sh
@@ -19,7 +19,7 @@ python ~/cdl/python/parallel_search.py -n 12  \
                                        -top_n 1000000  \
                                        -n_complete 15 \
                                        -n_cores 1000  \
-                                       -n_chunks 10000 \
+                                       -n_chunks 1000000 \
                                        -shuffle 1 \
                                        -rules "2N3" "2N1" "1N3" "3N1" \
                                        -lib_path "/data/home/acw554/cdl" \
