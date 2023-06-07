dcb88d4d3eecc39e85c94b25650dd4ac1cde8213
Bei Zhou <bei.zhou@qmul.ac.uk>
2023-06-07 04:03:53 +0100

    update

--- bind/windows_install.sh
+++ bind/windows_install.sh
@@ -3,6 +3,7 @@ cd build
 cmake ..
 
 cmake --build . --target ALL_BUILD --config Release
+rm D:\Anaconda3\Lib\site-packages\cdl.cp38-win_amd64.pyd
 
 mv ./Release/cdl.cp38-win_amd64.pyd D:\Anaconda3\Lib\site-packages\
 
