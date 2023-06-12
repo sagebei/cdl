332e33bf32648d7661c1d028ba7087af25346ff5
Bei Zhou <bei.zhou@qmul.ac.uk>
2023-06-12 03:21:02 +0100

    update

--- hpc/exceptions/calculate_sizes.sh
+++ hpc/exceptions/calculate_sizes.sh
@@ -13,7 +13,5 @@ module load gcc/12.1.0
 
 source ~/venv/bin/activate
 
-python ~/cdl/python/calculate_sizes.py -folder_path $1 \
-                                       -n_cores 100 \
-                                       -core_id ${SGE_TASK_ID}
+python ~/cdl/python/calculate_sizes.py -folder_path $1
 
