ec4dee6fdede6a7fe1e40c6e4c84489ed8af74d9
sagebei <bei.zhou@qmul.ac.uk>
2023-08-03 05:10:56 +0800

    update README

---
+++ python/databases/database_5.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#$ -cwd
+#$ -j y
+#$ -pe smp 1            # Request cores (8 per GPU)
+#$ -l h_vmem=32G         # 11G RAM per core
+#$ -l h_rt=24:0:0      # Max 1hr runtime (can request up to 240hr)
+#$ -N database
+
+module load python/3.8.5
+module load gcc/12.1.0
+
+source ~/venv/bin/activate
+
+python ~/cdl/python/databases/database_5.py 2N1 2N3 1N3 3N1 1N2 3N2
