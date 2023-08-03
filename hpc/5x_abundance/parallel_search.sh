fe0170c6bd932657047a585f271f08b372a4b43d
sagebei <bei.zhou@qmul.ac.uk>
2023-08-03 09:55:01 +0800

    update 5,x abundance

--- hpc/5x_abundance/parallel_search.sh
+++ hpc/5x_abundance/parallel_search.sh
@@ -14,10 +14,10 @@ module load gcc/12.1.0
 source ~/venv/bin/activate
 
 python ~/cdl/python/parallel_search.py -n $1  \
-                                       -cutoff 16  \
+                                       -cutoff $2  \
                                        -threshold 0  \
                                        -top_n -1  \
-                                       -n_complete $2 \
+                                       -n_complete -1 \
                                        -n_chunks 100 \
                                        -shuffle "." \
                                        -rules "2N1" "2N3" "1N2" "3N2" "1N3" "3N1" \
