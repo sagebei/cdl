e7abd4d8dd4e77053b56d37f466368b6e438982d
sagebei <bei.zhou@qmul.ac.uk>
2023-08-03 15:57:05 +0800

    update

--- hpc/4rules/complete_search.sh
+++ hpc/4rules/complete_search.sh
@@ -3,7 +3,7 @@
 #$ -cwd
 #$ -j y
 #$ -pe smp 1            # Request cores (8 per GPU)
-#$ -l h_vmem=8G         # 11G RAM per core
+#$ -l h_vmem=32G         # 11G RAM per core
 #$ -l h_rt=24:0:0      # Max 1hr runtime (can request up to 240hr)
 #$ -N complete
 
@@ -16,7 +16,7 @@ python ~/cdl/python/complete_search.py -n 8  \
                                        -cutoff 16  \
                                        -threshold 0  \
                                        -top_n 100000  \
-                                       -n_complete 20 \
+                                       -n_complete 15 \
                                        -n_chunks 10000 \
                                        -shuffle "." \
                                        -rules "2N1" "2N3" "1N3" "3N1" \
