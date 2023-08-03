46c7ec4e164d7f1f09bfc9ce92e27badb3cbcee8
sagebei <bei.zhou@qmul.ac.uk>
2023-08-03 15:44:21 +0800

    update

--- hpc/4rules/parallel_search.sh
+++ hpc/4rules/parallel_search.sh
@@ -5,7 +5,7 @@
 #$ -l h_vmem=8G
 #$ -j y
 #$ -l h_rt=24:0:0
-#$ -t 1-100
+#$ -t 1-1000
 #$ -N parallel
 
 module load python/3.8.5
@@ -16,9 +16,9 @@ source ~/venv/bin/activate
 python ~/cdl/python/parallel_search.py -n 8  \
                                        -cutoff 16  \
                                        -threshold 0  \
-                                       -top_n 10000  \
+                                       -top_n 100000  \
                                        -n_complete 20 \
-                                       -n_chunks 1000 \
+                                       -n_chunks 10000 \
                                        -shuffle "." \
                                        -rules "2N1" "2N3" "1N3" "3N1" \
                                        -lib_path "/data/home/acw554/cdl" \
