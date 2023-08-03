8d86eb240d25728661921f1661915bb4bf18ea2d
sagebei <bei.zhou@qmul.ac.uk>
2023-08-03 14:22:50 +0800

    add scripts for 4 rules

---
+++ hpc/4rules/complete_search.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#$ -cwd
+#$ -j y
+#$ -pe smp 1            # Request cores (8 per GPU)
+#$ -l h_vmem=8G         # 11G RAM per core
+#$ -l h_rt=24:0:0      # Max 1hr runtime (can request up to 240hr)
+#$ -N complete
+
+module load python/3.8.5
+module load gcc/12.1.0
+
+source ~/venv/bin/activate
+
+python ~/cdl/python/complete_search.py -n 8  \
+                                       -cutoff 16  \
+                                       -threshold 0  \
+                                       -top_n 10000  \
+                                       -n_complete 20 \
+                                       -n_chunks 1000 \
+                                       -shuffle "." \
+                                       -rules "2N1" "2N3" "1N3" "3N1" \
+                                       -lib_path "/data/home/acw554/cdl" \
+                                       -result_path "/data/scratch/acw554/4rules"
