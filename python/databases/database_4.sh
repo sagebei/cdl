9333e33fa0d774acbd5df251e622ef162d3fbfca
sagebei <bei.zhou@qmul.ac.uk>
2023-08-29 21:46:46 +0100

    change function

---
+++ python/databases/database_4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#$ -cwd
+#$ -j y
+#$ -pe smp 1            # Request cores (8 per GPU)
+#$ -l h_vmem=64G         # 11G RAM per core
+#$ -l h_rt=24:0:0      # Max 1hr runtime (can request up to 240hr)
+#$ -N database
+
+module load python/3.8.5
+module load gcc/12.1.0
+
+source ~/venv/bin/activate
+
+python ~/cdl/python/databases/database_4.py -rules 2N1 2N3 1N3 3N1 1N2 3N2
+
+
+
