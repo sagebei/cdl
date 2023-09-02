97a51fb8b067f5765eff56f91c353535c323517b
sagebei <bei.zhou@qmul.ac.uk>
2023-09-02 07:10:59 +0100

    update

--- python/databases/database_5.sh
+++ python/databases/database_5.sh
@@ -3,8 +3,8 @@
 #$ -cwd
 #$ -j y
 #$ -pe smp 1            # Request cores (8 per GPU)
-#$ -l h_vmem=64G         # 11G RAM per core
-#$ -l h_rt=24:0:0      # Max 1hr runtime (can request up to 240hr)
+#$ -l h_vmem=8G         # 11G RAM per core
+#$ -l h_rt=1:0:0      # Max 1hr runtime (can request up to 240hr)
 #$ -N database
 
 module load python/3.8.5
