3f1b0a4c0a0acde36ac44e2bd022a8460ae879d0
sagebei <bei.zhou@qmul.ac.uk>
2023-09-05 20:42:57 +0100

    update scripts

--- hpc/rsdfs/parallel_search.sh
+++ hpc/rsdfs/parallel_search.sh
@@ -16,14 +16,14 @@ source ~/venv/bin/activate
 python ~/cdl/python/RSDFS/parallel_search.py -n $1  \
                                              -cutoff 16  \
                                              -threshold 0  \
-                                             -n_complete 60 \
-                                             -n_chunks 10000 \
-                                             -per_trs_time_limit 180 \
+                                             -n_complete $2 \
+                                             -n_chunks 100 \
+                                             -per_trs_time_limit 5 \
                                              -chunk_size=1000 \
                                              -shuffle "." \
-                                             -rules "2N1" "2N3" \
+                                             -rules "1N3" "2N3" \
                                              -lib_path "/data/home/acw554/cdl" \
-                                             -result_path "/data/scratch/acw554/rsdfs" \
+                                             -result_path "/data/scratch/acw554/1n3_2n3" \
 
 
 
