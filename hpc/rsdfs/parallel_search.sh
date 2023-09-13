f40dc3f11af8197d6886868f59a44689fa691826
Bei Zhou <bei.zhou@qmul.ac.uk>
2023-09-13 15:44:28 +0100

    complete RSDFS

--- hpc/rsdfs/parallel_search.sh
+++ hpc/rsdfs/parallel_search.sh
@@ -22,10 +22,12 @@ python ~/cdl/python/RSDFS/parallel_search.py -n $1  \
                                              -chunk_size=1000 \
                                              -shuffle "." \
                                              -rules "1N3" "2N3" \
-                                             -lib_path "/data/home/acw554/cdl" \
-                                             -result_path "/data/scratch/acw554/1n3_2n3" \
                                              -n_cores=200 \
                                              -core_id ${SGE_TASK_ID} \
+                                             -split_depth 5 \
+                                             -lib_path "/data/home/acw554/cdl" \
+                                             -result_path "/data/scratch/acw554/1n3_2n3" \
+
 
 
 
